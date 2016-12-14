72964aa9118d9ed696606cb72f5c63f8de82b2a6
Kevin Gray <graykevinb@gmail.com>
2016-12-13 21:50:48 -0600

    Create rotate.py

---
+++ rotate.py
@@ -0,0 +1,36 @@
+from BrickPi import *
+BrickPiSetup()
+port = PORT_C
+BrickPi.MotorEnable[port] = 1
+
+def rotateMotor(power, deg, port):
+    BrickPiUpdateValues()
+    enc = BrickPi.Encoder[port]
+    if deg > 0:
+        target_pos = enc + deg
+        while enc < target_pos:
+            BrickPiUpdateValues()
+            enc = BrickPi.Encoder[port]
+            BrickPi.MotorSpeed[port] = power
+        motorStop(port, '+')
+    else:
+        target_pos = enc - deg
+        while enc > deg:
+            BrickPiUpdateValues()
+            enc = BrickPi.Encoder[port]
+            BrickPi.MotorSpeed[port] = -power
+        motorStop(port, '-')
+
+def motorStop(port, direction):
+    """breaks the specified motor. put + for forwards or -,
+    direction must be specified so that the motor can break."""
+    if direction == '+':
+        power = -10
+    else:
+        power = 10
+    print("breaking")
+    for i in range(0, 2):
+        BrickPiUpdateValues()
+        BrickPi.MotorSpeed[port] = power
+
+rotateMotor(55, 70, port)
